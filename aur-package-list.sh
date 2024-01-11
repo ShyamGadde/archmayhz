e0d9a3020231e9ffb73d19a2e066781175d42490
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-01-11 10:29:22 +0000

    Update aur-package-list.sh

--- aur-package-list.sh
+++ aur-package-list.sh
@@ -1,7 +1,10 @@
 AUR_PACKAGES=(
     #iwgtk # GUI for iwctl
+    btrfs-assistant
     duf # Disk Usage/Free Utility
     lf-bin
     microsoft-edge-stable-bin
+    snapper-support
+    snapper-tools
     visual-studio-code-bin
 )
