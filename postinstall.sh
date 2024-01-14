357ec7b7074a2ad044ce71034374f68c1103bbef
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-01-14 19:09:05 +0530

    Add new configuration files

--- postinstall.sh
+++ postinstall.sh
@@ -9,7 +9,7 @@ print_info "ENABLING UFW..."
 sudo ufw enable
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
-sudo ufw allow ssh
+sudo ufw limit ssh
 
 print_info "ENABLING PIPEWIRE..."
 systemctl --user enable --now pipewire
@@ -17,7 +17,7 @@ systemctl --user enable --now pipewire-pulse
 systemctl --user enable --now wireplumber
 
 print_info "UPDATING PLOCATE DATABASE..."
-sudo cp /etc/updatedb.conf /etc/updatedb.conf.bak
+backup_file /etc/updatedb.conf
 sudo sed -i '/^PRUNENAMES/s/"$/ .snapshots"/' /etc/updatedb.conf # Ignore BTRFS snapshots
 sudo updatedb
 
@@ -28,6 +28,7 @@ print_info "INSTALLING PARU..."
 git clone https://aur.archlinux.org/paru-bin.git ~/repos/paru-bin
 cd ~/repos/paru-bin
 makepkg -si --noconfirm
+apply_config /etc/paru.conf
 cd ~
 
 source <(curl -fsSL https://raw.githubusercontent.com/ShyamGadde/archmayhz/main/aur-package-list.sh)
@@ -38,15 +39,32 @@ for package in "${AUR_PACKAGES[@]}"; do
     paru -S "${package}"
 done
 
-# ---------------------------- #
-# ---------- SSH ------------- #
-# ---------------------------- #
-print_info "SETTING UP SSH KEYS..."
-export BW_SESSION=$(bw login --raw)
-mkdir -p ~/.ssh
-bw get notes 79984437-d2f8-4483-804f-b0eb00720fca >~/.ssh/github_ed25519
-chmod 600 ~/.ssh/github_ed25519
-bw get notes 6bd9e32b-0c2d-4273-925e-b0eb00724b7b >~/.ssh/github_ed25519.pub
-chmod 644 ~/.ssh/github_ed25519.pub
+print_info "CONFIGURING APPLICATIONS..."
+# Fix Anki text pixelated on qt6 with wayland by using xwayland
+apply_config /usr/local/bin/anki
+chmod +x /usr/local/bin/anki
+
+# Make applications run on wayland instead.
+# Obsidian
+apply_config /usr/local/bin/obsidian
+chmod +x /usr/local/bin/obsidian
+sed -i 's|Exec=/usr/bin/obsidian\(.*\)|Exec=obsidian\1|g' /usr/share/applications/obsidian.desktop
+# Microsoft Edge
+apply_config /usr/local/bin/microsoft-edge-stable
+chmod +x /usr/local/bin/microsoft-edge-stable
+sed -i 's|Exec=/usr/bin/microsoft-edge-stable\(.*\)|Exec=microsoft-edge-stable\1|g' /usr/share/applications/microsoft-edge.desktop
+
+# TODO: Configure Snapper
+# 1. Root config
+# 2. Home config
+
+# TODO: Downlaod dotfiles
+
+# TODO: Setup GitHub CLI
+
+# Theming
+papirus-folders -C cat-mocha-blue --theme Papirus-Dark
+
+# TODO: Create root and home snapshots of **Base System Installation**
 
 echo "DONE!" | figlet -f slant | lolcat
